ce60361f29e9fe125dd96d58938bba3a47356475
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-02-07 14:00:26 +0200

    add Spring configuration
    add Spring configuration and implementation

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,11 +8,19 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-  private SpeakerRepository speakerRepository = new StubSpeakerRepositoryImpl();
+  private SpeakerRepository speakerRepository;
+
+  public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+    this.speakerRepository = speakerRepository;
+  }
 
   @Override
   public List<Speaker> findAll() {
     return speakerRepository.findAll();
+  }
+
+  public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+    this.speakerRepository = speakerRepository;
   }
 
 }
